dd70519a21c927b94d8fb58dfcf0351575943492
Gordon <gordonfgz@gmail.com>
2020-09-18 20:07:39 +0800

    Add BloodType class

---
+++ src/main/java/seedu/address/model/patient/BloodType.java
@@ -0,0 +1,60 @@
+package seedu.address.model.patient;
+
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class BloodType {
+
+    private static final String MESSAGE_CONSTRAINTS =
+            "BloodType must be valid. Eg: A+";
+
+    private static final String[] Types = { "A+", "A-", "B+", "B-", "O", "O+", "O-", "AB+", "AB-"};
+
+    public final String value;
+
+    /**
+     * Constructs a {@code BloodType}.
+     *
+     * @param bloodtype A valid bloodtype number.
+     */
+    public BloodType(String bloodtype) {
+        requireNonNull(bloodtype);
+        checkArgument(isValidBloodType(bloodtype), MESSAGE_CONSTRAINTS);
+        value = bloodtype;
+    }
+
+    /**
+     * Returns true if a given string is a valid birthdate number.
+     */
+    public static boolean isValidBloodType(String test) {
+        for (String validType: Types) {
+            if (test.equals(validType)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof BloodType // instanceof handles nulls
+                && value.equals(((BloodType) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
