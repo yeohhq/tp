9093185b575a76d8dc5f4f9b22f22534958cc7bd
Gordon <gordonfgz@gmail.com>
2020-10-05 16:05:45 +0800

    Edit description code not affected

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -34,7 +34,7 @@ import seedu.address.model.patient.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
- * Edit the details of an existing patient in the address book.
+ * Edits the details of an existing patient in the address book.
  */
 public class EditCommand extends Command {
 
