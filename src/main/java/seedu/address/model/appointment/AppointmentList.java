7e33f416d5fd7fd2f7457659f73dee3b96569c5f
chshen1998 <chewhoashen@gmail.com>
2020-10-07 15:49:05 +0800

    Add Appointment Exceptions

--- src/main/java/seedu/address/model/appointment/AppointmentList.java
+++ src/main/java/seedu/address/model/appointment/AppointmentList.java
@@ -11,19 +11,132 @@ import javafx.collections.ObservableList;
 import seedu.address.model.appointment.exceptions.DuplicateAppointmentException;
 import seedu.address.model.appointment.exceptions.AppointmentNotFoundException;
 import seedu.address.model.patient.Patient;
+import seedu.address.model.patient.UniquePatientList;
+import seedu.address.model.patient.exceptions.DuplicatePatientException;
+import seedu.address.model.patient.exceptions.PatientNotFoundException;
 
 /**
  * A list of appointments that enforces uniqueness between its elements and does not allow nulls.
  * An appointment is considered unique by comparing using {@code Appointment#isSameAppointment(Appointment)}.
  * As such, adding and updating of appointments uses Appointment#isSameAppointment(Appointment)
- * for equality so as to ensure that the patient being added or updated is
- * unique in terms of identity in the UniquePatientList.
- * However, the removal of a patient uses Patient#equals(Object) so
- * as to ensure that the patient with exactly the same fields will be removed.
+ * for equality so as to ensure that the appointment being added or updated is
+ * unique in terms of identity in the AppointmentList.
+ * However, the removal of an appointment uses Appointment#equals(Object) so
+ * as to ensure that the appointment with exactly the same fields will be removed.
  *
  * Supports a minimal set of list operations.
  *
- * @see Patient#isSamePatient(Patient)
+ * @see Appointment#isSameAppointment(Appointment)
  */
-public class AppointmentList {
+public class AppointmentList implements Iterable<Appointment {
+
+    private ObservableList<Appointment> internalList = FXCollections.observableArrayList();
+    private final ObservableList<Appointment> internalUnmodifiableList =
+            FXCollections.unmodifiableObservableList(internalList);
+
+    /**
+     * Returns true if the list contains an equivalent patient as the given argument.
+     */
+    public boolean contains(Patient toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::isSameAppointment);
+    }
+
+    /**
+     * Adds a patient to the list.
+     * The patient must not already exist in the list.
+     */
+    public void add(Patient toAdd) {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicatePatientException();
+        }
+        internalList.add(toAdd);
+    }
+
+    /**
+     * Replaces the patient {@code target} in the list with {@code editedPatient}.
+     * {@code target} must exist in the list.
+     * The patient identity of {@code editedPatient} must not be the same as another existing patient in the list.
+     */
+    public void setPatient(Patient target, Patient editedPatient) {
+        requireAllNonNull(target, editedPatient);
+
+        int index = internalList.indexOf(target);
+        if (index == -1) {
+            throw new PatientNotFoundException();
+        }
+
+        if (!target.isSamePatient(editedPatient) && contains(editedPatient)) {
+            throw new DuplicatePatientException();
+        }
+
+        internalList.set(index, editedPatient);
+    }
+
+    /**
+     * Removes the equivalent patient from the list.
+     * The patient must exist in the list.
+     */
+    public void remove(Patient toRemove) {
+        requireNonNull(toRemove);
+        if (!internalList.remove(toRemove)) {
+            throw new PatientNotFoundException();
+        }
+    }
+
+    public void setPatients(UniquePatientList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
+    /**
+     * Replaces the contents of this list with {@code patients}.
+     * {@code patients} must not contain duplicate patients.
+     */
+    public void setPatients(List<Patient> patients) {
+        requireAllNonNull(patients);
+        if (!personsAreUnique(patients)) {
+            throw new DuplicatePatientException();
+        }
+        internalList.setAll(patients);
+    }
+
+    /**
+     * Returns the backing list as an unmodifiable {@code ObservableList}.
+     */
+    public ObservableList<Patient> asUnmodifiableObservableList() {
+        return internalUnmodifiableList;
+    }
+
+    @Override
+    public Iterator<Patient> iterator() {
+        return internalList.iterator();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof UniquePatientList // instanceof handles nulls
+                && internalList.equals(((UniquePatientList) other).internalList));
+    }
+
+    @Override
+    public int hashCode() {
+        return internalList.hashCode();
+    }
+
+    /**
+     * Returns true if {@code patients} contains only unique patients.
+     */
+    private boolean personsAreUnique(List<Patient> patients) {
+        for (int i = 0; i < patients.size() - 1; i++) {
+            for (int j = i + 1; j < patients.size(); j++) {
+                if (patients.get(i).isSamePatient(patients.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
