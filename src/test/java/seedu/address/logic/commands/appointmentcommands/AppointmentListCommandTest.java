e4cfba859a6239b348b1675c79b6eeb61dd65da2
chshen1998 <chewhoashen@gmail.com>
2020-10-08 22:33:29 +0800

    Fix checkstyles

--- src/test/java/seedu/address/logic/commands/appointmentcommands/AppointmentListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/appointmentcommands/AppointmentListCommandTest.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.commands.appointmentcommands;
 
+import static seedu.address.logic.commands.patientcommands.PatientCommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.patientcommands.PatientListCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.logic.commands.patientcommands.PatientCommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for AppointmentListCommand.
@@ -27,8 +28,8 @@ public class AppointmentListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new AppointmentListCommand(), model
-                , AppointmentListCommand.MESSAGE_LIST_APPOINTMENT_SUCCESS, expectedModel);
+        assertCommandSuccess(new AppointmentListCommand(), model,
+                AppointmentListCommand.MESSAGE_LIST_APPOINTMENT_SUCCESS, expectedModel);
     }
 
     @Test
