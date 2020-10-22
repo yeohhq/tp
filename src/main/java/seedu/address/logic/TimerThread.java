769b6415259e409c866ddfbd0e72e971eee70430
chshen1998 <chewhoashen@gmail.com>
2020-10-22 13:57:49 +0800

    Add javadocs

--- src/main/java/seedu/address/logic/TimerThread.java
+++ src/main/java/seedu/address/logic/TimerThread.java
@@ -3,16 +3,26 @@ package seedu.address.logic;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Thread used to check for newly missed appointments at regular intervals.
+ */
 public class TimerThread extends Thread {
 
     private Logic logic;
     private Boolean running;
 
+    /**
+     * Constructor for thread.
+     * @param logic
+     */
     public TimerThread(Logic logic) {
         this.logic = logic;
         this.running = true;
     }
 
+    /**
+     * Executes the AppointmentNesMissesCommand at regular intervals.
+     */
     public void run() {
         while (running) {
             try {
