d5c033f5bea02db80e74c0d241055cc9135a2560
soaza <65228562+soaza@users.noreply.github.com>
2020-10-27 16:24:18 +0800

    Implement Suggestion Box in GUI

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,12 +1,24 @@
 package seedu.address.ui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.*;
+
+import static seedu.address.commons.util.SuggestionBoxUtil.createSuggestions;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
@@ -17,6 +29,8 @@ public class CommandBox extends UiPart<Region> {
     private static final String FXML = "CommandBox.fxml";
 
     private final CommandExecutor commandExecutor;
+    private ContextMenu suggestionPopup;
+    private final SortedSet<String> suggestions;
 
     @FXML
     private TextField commandTextField;
@@ -24,11 +38,73 @@ public class CommandBox extends UiPart<Region> {
     /**
      * Creates a {@code CommandBox} with the given {@code CommandExecutor}.
      */
+
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        /**Source code with some modifications
+         * author: https://gist.github.com/floralvikings
+         * https://gist.github.com/floralvikings/10290131
+         */
+        suggestionPopup = new ContextMenu();
+        suggestions = new TreeSet<>();
+        suggestions.addAll(createSuggestions());
+        commandTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                if(commandTextField.getText().length() == 0) {
+                    suggestionPopup.hide();
+                } else
+                {
+                    LinkedList<String> searchResult = new LinkedList<>();
+                    searchResult.addAll(suggestions.subSet(commandTextField.getText(),
+                            commandTextField.getText() + Character.MAX_VALUE));
+                    if (suggestions.size() > 0) {
+                        populatePopup(searchResult);
+                        if(!suggestionPopup.isShowing()) {
+                            suggestionPopup.show(commandTextField, Side.BOTTOM,0,0);
+                        } else {
+                            suggestionPopup.hide();
+                        }
+                    }
+                }
+            }
+        });
+        commandTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean aBoolean, Boolean aBoolean2) {
+                suggestionPopup.hide();
+            }
+        });
+    }
+
+    public SortedSet<String> getSuggestions() { return suggestions; }
+
+    private void populatePopup(List<String> searchResult) {
+        List<CustomMenuItem> menuItems = new LinkedList<>();
+        // If you'd like more entries, modify this line.
+        int maxEntries = 5;
+        int count = Math.min(searchResult.size(), maxEntries);
+        for (int i = 0; i < count; i++)
+        {
+            final String result = searchResult.get(i);
+            Label entryLabel = new Label(result);
+            CustomMenuItem item = new CustomMenuItem(entryLabel, true);
+//            item.setOnAction(new EventHandler<ActionEvent>()
+//            {
+//                @Override
+//                public void handle(ActionEvent actionEvent) {
+//                    commandTextField.setText(result);
+//                    suggestionPopup.hide();
+//                }
+//            });
+            menuItems.add(item);
+        }
+        suggestionPopup.getItems().clear();
+        suggestionPopup.getItems().addAll(menuItems);
+
     }
 
     /**
