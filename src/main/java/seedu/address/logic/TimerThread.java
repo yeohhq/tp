35a792753a148c9ae75268a7a548386eb63d69cf
chshen1998 <chewhoashen@gmail.com>
2020-10-27 18:25:26 +0800

    Update thread

--- src/main/java/seedu/address/logic/TimerThread.java
+++ src/main/java/seedu/address/logic/TimerThread.java
@@ -40,7 +40,7 @@ public class TimerThread extends Thread {
                 e.printStackTrace();
             }
             try {
-                Thread.sleep(8000);
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
