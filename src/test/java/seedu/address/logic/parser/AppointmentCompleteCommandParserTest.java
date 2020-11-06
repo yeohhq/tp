150118f8ad5ca4ba0a723d618db4dda6b6ab3963
Gordon <gordonfgz@gmail.com>
2020-11-06 22:09:18 +0800

    B

--- src/test/java/seedu/address/logic/parser/AppointmentCompleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AppointmentCompleteCommandParserTest.java
@@ -9,36 +9,29 @@ import static seedu.address.logic.commands.appointmentcommands.AppointmentComman
 import static seedu.address.logic.commands.appointmentcommands.AppointmentCommandTestUtil.VALID_PATIENT_ONE;
 import static seedu.address.logic.commands.appointmentcommands.AppointmentCommandTestUtil.VALID_TAG_ONE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_APPOINTMENT;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.appointmentcommands.AppointmentScheduleCommand;
-import seedu.address.logic.parser.appointmentparser.AppointmentScheduleCommandParser;
+import seedu.address.logic.commands.appointmentcommands.AppointmentCompleteCommand;
+import seedu.address.logic.parser.appointmentparser.AppointmentCompleteCommandParser;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.appointment.Appointment;
 import seedu.address.testutil.AppointmentBuilder;
 
-public class AppointmentScheduleCommandParserTest {
-    private AppointmentScheduleCommandParser parser = new AppointmentScheduleCommandParser();
+public class AppointmentCompleteCommandParserTest {
+    private AppointmentCompleteCommandParser parser = new AppointmentCompleteCommandParser();
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void parse_allFieldsPresent_success() {
-        String expectedPatientString = VALID_PATIENT_ONE;
 
-        Appointment expectedAppointment = new AppointmentBuilder()
-                            .withPatientString(expectedPatientString)
-                            .withTags(VALID_TAG_ONE).buildAppointmentWithPatientString();
-
-        AppointmentScheduleCommand appointmentScheduleCommand = new AppointmentScheduleCommand(expectedAppointment);
 
         // whitespace only preamble
-        assertParseSuccess(parser, PREAMBLE_WHITESPACE + START_DESC_ONE + END_DESC_ONE
-                + PATIENT_DESC_ONE + DESCRIPTION_DESC_ONE
-                + TAG_DESC_ONE, appointmentScheduleCommand);
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE+""), appointmentCompleteCommand);
     }
 }
